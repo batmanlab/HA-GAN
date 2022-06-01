350c45d003dd0c6d88fc4b10c6ba68a44328d83c
Li Sun <lisun97@foxmail.com>
2022-05-31 21:49:47 -0400

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,4 @@
 # resize and rescale images for preprocessing
-# expect input images have a suffix of .nii.gz
 
 import glob
 import SimpleITK as sitk
@@ -8,29 +7,35 @@ from skimage.transform import resize
 import os
 import multiprocessing as mp
 
+### Configs
 # 8 cores are used for multi-thread processing
 NUM_JOBS = 8
-# output size
+#  resized output size, can be 128 or 256
 IMG_SIZE = 256
 INPUT_DATA_DIR = '/path_to_imgs/'
 OUTPUT_DATA_DIR = '/output_folder/'
-# the intensity range is clipped with the two thresholds
+# the intensity range is clipped with the two thresholds, this default is used for our CT images, please adapt to your own dataset
 LOW_THRESHOLD = -1024
 HIGH_THRESHOLD = 600
+# suffix (ext.) of input images
+SUFFIX = '.nii.gz'
+# whether or not to trim blank axial slices, recommend to set as True
+TRIM_BLANK_SLICES = True
 
 def resize_img(img):
     nan_mask = np.isnan(img) # Remove NaN
     img[nan_mask] = LOW_THRESHOLD
     img = np.interp(img, [LOW_THRESHOLD, HIGH_THRESHOLD], [-1,1])
 
-    valid_plane_i = np.mean(img, (1,2)) != -1 # Remove blank axial planes
-    img = img[valid_plane_i,:,:]
+    if TRIM_BLANK_SLICES:
+        valid_plane_i = np.mean(img, (1,2)) != -1 # Remove blank axial planes
+        img = img[valid_plane_i,:,:]
 
     img = resize(img, (IMG_SIZE, IMG_SIZE, IMG_SIZE), mode='constant', cval=-1)
     return img
 
 def main():
-    img_list = list(glob.glob(INPUT_DATA_DIR+"*.nii.gz"))
+    img_list = list(glob.glob(INPUT_DATA_DIR+"*"+SUFFIX))
 
     processes = []
     for i in range(NUM_JOBS):
@@ -44,7 +49,7 @@ def batch_resize(batch_idx, img_list):
             continue
         imgname = img_list[idx].split('/')[-1]
         if os.path.exists(OUTPUT_DATA_DIR+imgname.split('.')[0]+".npy"):
-            # skip already finished pre-processing
+            # skip images that already finished pre-processing
             continue
         try:
             img = sitk.ReadImage(INPUT_DATA_DIR + img_list[idx])
@@ -59,7 +64,8 @@ def batch_resize(batch_idx, img_list):
         except Exception as e: # Some images are corrupted
             print(e)
             print("Image resize error:", imgname)
-            continue 
+            continue
+        # preprocessed images are saved in numpy arrays
         np.save(OUTPUT_DATA_DIR+imgname.split('.')[0]+".npy", img)
 
 if __name__ == '__main__':
